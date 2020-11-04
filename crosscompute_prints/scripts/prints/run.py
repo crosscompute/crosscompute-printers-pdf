b0f06da19fe31a65a1a18f8b35b91c273ef65b9a
Roy Hyunjin Han <rhh@crosscompute.com>
2020-11-03 21:38:16 -0500

    Support document_name

--- crosscompute_prints/scripts/prints/run.py
+++ crosscompute_prints/scripts/prints/run.py
@@ -1,53 +1,53 @@
+# TODO: Consider print chore query in case echoes are missed
+# TODO: Consider print chore query for parallelization
+# TODO: Consider merging into crosscompute workers run
 import asyncio
 import json
 import requests
+from crosscompute.routines import (
+    get_client_url, get_echoes_client)
 from crosscompute.scripts import AuthenticatingScript
 from invisibleroads_macros_disk import (
     TemporaryStorage, archive_safely, make_folder)
 from os.path import join
 from pyppeteer import launch
-from sseclient import SSEClient
-
-
-# !!!
-CLIENT_HOST = 'https://projects.iixyfqfy.crosscompute.com'
 
 
 class RunPrinterScript(AuthenticatingScript):
 
     def run(self, args, argv):
         super().run(args, argv)
-        return run(args.host, args.token)
+        client_url = get_client_url()
+        return run(args.server_url, args.server_token, client_url)
 
 
-def get_echoes_client(host, token):
-    url = f'{host}/echoes/{token}.json'
-    return SSEClient(url)
-
-
-def run(host, token):
-    echoes_client = get_echoes_client(host, token)
+def run(server_url, server_token, client_url):
+    echoes_client = get_echoes_client(server_url, server_token)
     for echo_message in echoes_client:
         print(echo_message.__dict__)
         if echo_message.event == 'w':
             d = json.loads(echo_message.data)
             print_id = d['x']
-            document_count = d['#']
             file_url = d['@']
-            print(print_id, document_count)
-            asyncio.run(do(print_id, document_count, file_url))
+            asyncio.run(do(print_id, server_url, client_url, file_url))
 
 
-async def do(print_id, document_count, file_url):
+async def do(print_id, server_url, client_url, file_url):
+    url = f'{server_url}/prints/{print_id}.json'
+    response = requests.get(url)
+    print_dictionary = response.json()
+    document_dictionaries = print_dictionary['documents']
     browser = await launch()
     page = await browser.newPage()
     with TemporaryStorage() as storage:
         storage_folder = storage.folder
         documents_folder = make_folder(join(storage_folder, 'documents'))
-        for document_index in range(document_count):
-            url = f'{CLIENT_HOST}/prints/{print_id}/documents/{document_index}'
-            print(url)
-            target_path = join(documents_folder, f'{document_index}.pdf')
+        for document_index, document_dictionary in enumerate(
+                document_dictionaries):
+            target_name = document_dictionary['name']
+            target_path = join(documents_folder, target_name + '.pdf')
+            url = f'{client_url}/prints/{print_id}/documents/{document_index}'
+            print(url, target_path)
             await page.goto(url, {'waitUntil': 'networkidle2'})
             await page.pdf({'path': target_path})
         archive_path = archive_safely(documents_folder)
