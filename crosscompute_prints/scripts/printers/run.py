a3daa2340f0e054378fea4ed8a1a29b6e4115819
Roy Hyunjin Han <rhh@crosscompute.com>
2021-02-09 16:44:02 +0000

    Start new browser for each document

--- crosscompute_prints/scripts/printers/run.py
+++ crosscompute_prints/scripts/printers/run.py
@@ -59,13 +59,14 @@ async def do(print_id, file_url):
     response = requests.get(url)
     print_dictionary = response.json()
     document_dictionaries = print_dictionary['documents']
-    browser = await launch()
-    page = await browser.newPage()
     with TemporaryStorage() as storage:
         storage_folder = storage.folder
         documents_folder = make_folder(join(storage_folder, 'documents'))
         for document_index, document_dictionary in enumerate(
                 document_dictionaries):
+            browser = await launch()
+            page = await browser.newPage()
+
             target_name = document_dictionary['name']
             target_path = join(documents_folder, target_name + '.pdf')
             url = f'{client_url}/prints/{print_id}/documents/{document_index}'
@@ -81,9 +82,8 @@ async def do(print_id, file_url):
                     page = await browser.newPage()
 
             await page.pdf({'path': target_path, 'printBackground': True})
+            await browser.close()
         archive_path = archive_safely(documents_folder)
         with open(archive_path, 'rb') as data:
             response = requests.put(file_url, data=data)
             print(response.__dict__)
-    await browser.close()
-    # os.system('pkill -9 chrome')
